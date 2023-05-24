be1b69d755f17774098c35ca5676bb2a7666eb23
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-24 19:03:18 -0300

    sexto requisito completo

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const memeImg = document.getElementById('imagem');
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
+const memeContainer = document.getElementById('meme-image-container');
 
 textInput.addEventListener('keyup', () => {
   memeText.innerText = textInput.value;
@@ -12,3 +16,14 @@ let loadFile = function (event) {
     URL.revokeObjectURL(memeImg.src);
   };
 };
+
+fire.addEventListener('click', () => {
+  memeContainer.style.border = '3px dashed rgb(255, 0, 0)';
+});
+water.addEventListener('click', () => {
+  memeContainer.style.border = '5px double rgb(0, 0, 255)';
+});
+
+earth.addEventListener('click', () => {
+  memeContainer.style.border = '6px groove rgb(0, 128, 0)';
+});
