f7c5efbcc227ef051d2c54a5c12dda94cd7a53e7
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-25 15:07:07 -0300

    vai luquinha

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
-const memeImg = document.getElementById('imagem');
+const memeImg = document.getElementById('meme-image');
 const fire = document.getElementById('fire');
 const water = document.getElementById('water');
 const earth = document.getElementById('earth');
 const memeContainer = document.getElementById('meme-image-container');
 const meme = document.getElementById('meme');
-const memeImg2 = document.getElementById('meme-image');
 
 textInput.addEventListener('keyup', () => {
   memeText.innerText = textInput.value;
@@ -29,7 +28,7 @@ water.addEventListener('click', () => {
 earth.addEventListener('click', () => {
   memeContainer.style.border = '6px groove rgb(0, 128, 0)';
 });
+
 meme.addEventListener('click', (event) => {
-  memeImg.src = event.target.src;
-  memeImg2.setAttribute('src', event.target.src);
+memeImg.src = event.target.src;
 });
