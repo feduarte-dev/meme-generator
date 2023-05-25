af4e2c716223af6fe6d695ff7f90ef557c35dca0
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-25 12:11:00 -0300

    todos requisitos prontos

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
+// const textInput = document.getElementById('text-input');
+// const memeText = document.getElementById('meme-text');
+
+// const fire = document.getElementById('fire');
+// const water = document.getElementById('water');
+// const earth = document.getElementById('earth');
+// const memeContainer = document.getElementById('meme-image-container');
+// 
+
+// textInput.addEventListener('keyup', () => {
+//   memeText.innerText = textInput.value;
+// });
+
+// const addImg = () => {
+//   const imgTag = document.createElement('img');
+//   imgTag.setAttribute('id', 'meme-image');
+//   memeContainer.appendChild(imgTag);
+// };
+// addImg();
+// const memeImg = document.getElementById('meme-image');
+
+// // Ensinada nas dicas do readme
+// let loadFile = function (event) {
+//   memeImg.src = URL.createObjectURL(event.target.files[0]);
+//   memeImg.onload = function () {
+//     URL.revokeObjectURL(memeImg.src);
+//   };
+// };
+
+// fire.addEventListener('click', () => {
+//   memeContainer.style.border = '3px dashed rgb(255, 0, 0)';
+// });
+// water.addEventListener('click', () => {
+//   memeContainer.style.border = '5px double rgb(0, 0, 255)';
+// });
+
+// earth.addEventListener('click', () => {
+//   memeContainer.style.border = '6px groove rgb(0, 128, 0)';
+// });
+
+
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
-
+const memeImg = document.getElementById('imagem');
 const fire = document.getElementById('fire');
 const water = document.getElementById('water');
 const earth = document.getElementById('earth');
 const memeContainer = document.getElementById('meme-image-container');
 const meme = document.getElementById('meme');
+const memeImg2 = document.getElementById('meme-image');
 
 textInput.addEventListener('keyup', () => {
   memeText.innerText = textInput.value;
 });
-
-const addImg = () => {
-  const imgTag = document.createElement('img');
-  imgTag.setAttribute('id', 'meme-image');
-  memeContainer.appendChild(imgTag);
-};
-addImg();
-const memeImg = document.getElementById('meme-image');
-
 // Ensinada nas dicas do readme
 let loadFile = function (event) {
   memeImg.src = URL.createObjectURL(event.target.files[0]);
@@ -37,7 +70,7 @@ water.addEventListener('click', () => {
 earth.addEventListener('click', () => {
   memeContainer.style.border = '6px groove rgb(0, 128, 0)';
 });
-
 meme.addEventListener('click', (event) => {
   memeImg.src = event.target.src;
+memeImg2.setAttribute('src', event.target.src)
 });
